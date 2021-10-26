5bb48f358c4baff3f5eb4f18dd4d9c3174a59862
iqbalmdkaify <iqbalmdkaify81@gmail.com>
2021-10-27 02:29:19 +0300

    change

--- level12.py
+++ level12.py
@@ -5,15 +5,15 @@ def sortByHeight(a):
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
 
-itr_a, itr_b, next = a[0], a[0], a[0]
+indexing_length = len(a)-1
+sorted = False
 
-for i in range(len(a)+1):
-    if a[i] == -1:
-        itr_a = a[i]
-    elif a[i]!=-1:
-        itr_b = a[i]
-        if a[i+1]!=-1 and a[i]!=a[-1]:
-            if a[i+1]>itr_b:
-                a[i], a[i+1] = a[i+1], a[i]
+while not sorted:
+    sorted = True
+    for i in range(0,indexing_length):
+        if a[i]>a[i+1] and all([a[i]!=-1, a[i+1]!=-1]):
+            sorted = False
+            a[i], a[i+1] = a[i+1], a[i]
+
 print(a)
 
