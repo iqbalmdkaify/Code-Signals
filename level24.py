ff859e51544c677d0361a4fcc23aa4a5a54ff242
iqbalmdkaify <iqbalmdkaify81@gmail.com>
2022-01-27 00:47:29 +0300

    done

--- level24.py
+++ level24.py
@@ -3,28 +3,27 @@
 
 def solution(matrix):
 
-    r_mat = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
+    r_mat = []
     count = 0
 
     for j in range(len(matrix)):
+        r_mat.append([])
         nest = r_mat[j]
         for k in range(len(matrix[j])):
-            # print(f'k  {k}')
 
             # 2nd loop
             for row in range(len(matrix)):
                 for col in range(len(matrix[row])):
 
-                   if any([(row==j), (row==j+1), (row==j-1)]) and any([(col==k), (col==k+1), (col==k-1)]):
 
-                       if matrix[row][col]==1 and (row, col)!=(j, k):
-                           count+=1
-                           print(f' found:    x {row}   y {col}\n')
+                    if any([(row==j), (row==j+1), (row==j-1)]) and any([(col==k), (col==k+1), (col==k-1)]):
+
+                        if matrix[row][col]==1 and (row, col)!=(j, k):
+                            count+=1
+                            print(f' found:    x {row}   y {col}\n')
 
             print(f'row {j}   col {k}    count {count}')
-            nest[k]=count
+            nest.insert(k, count)
             count = 0
 
             
