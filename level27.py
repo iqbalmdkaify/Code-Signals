e81edcb8aea2a220a1f964374e6b33b9a3d43dad
iqbalmdkaify <iqbalmdkaify81@gmail.com>
2022-03-05 13:08:12 +0300

    done 27

--- level27.py
+++ level27.py
@@ -2,20 +2,41 @@
 
 # Check if the given string is a correct variable name.
 
-def solution(name):
-    res = []
-    if name == "":
-        return False
-    for i in range(len(name)):
+# this solution has errors 230/300
+
+# def solution(name):
+#     res = []
+#     if name.isspace():
+#         return False
+#     for i in range(len(name)):
         
-        if i==0:
-            if name[0].isdigit() or name[0].isspace():
-                res.append(False)
-        elif i>0:
-            if name[i]=='_' or name[i].isalnum():
-                res.append(True)
-            elif name[i]!='_' or not(name[i].isalnum()) or name[i].isspace():
-                res.append(False)
+#         if i==0:
+#             if name[0].isdigit() or name[0].isspace():
+#                 res.append(False)
+#         elif i>0:
+#             if name[i]=='_' or name[i].isalnum():
+#                 res.append(True)
+#             elif name[i]!='_' or not(name[i].isalnum()) or name[i].isspace():
+#                 res.append(False)
         
     
-    return all(res)
+#     return all(res)
+
+# alternate solution
+
+def solution(name):
+    var = True
+
+    if name.isspace():
+        return False
+    elif name == '':
+        return False
+    elif name[0].isdigit():
+        return False
+
+    ls = [k for k in name]
+    for k in range(len(ls)):
+        if not (ls[k].isalpha() or ls[k].isdigit() or ls[k]=="_"):
+            var = False
+
+    return var
--- level56.py
+++ level56.py
@@ -2,23 +2,4 @@
 
 
 def solution(product):
-    maps = {}
-    f_str = ''
-    min_l = product
-    
-    if len(str(product))==1 and product>0:
-        return int("1"+str(product))
-    for k in range(1, 10):
-        if product%k!=0:
-            continue
-        elif product%k==0:
-            n = int(str(k)+str(product//2))
-            if n<min_l:
-                f_str += str(k)+str(product//2)
-                min_l = n
-
-    
-
-    return f_str
-
-print(solution(12))
+    pass
